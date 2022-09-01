c2a8bef0a6f0aa45beed3674bc3d71cefa546dc2
Stephanie Sanchez <stephk006@gmail.com>
2022-09-01 13:40:02 -0400

    created cards array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 
+const cardImages = [
+  { src: "/images/blank.png" },
+  { src: "/images/cane-corso.png" },
+  { src: "/images/chichuahua.png" },
+  { src: "/images/doberman.png" },
+  { src: "/images/german-shepherd.png" },
+  { src: "/images/poodle.png" },
+  { src: "/images/saint-bernard.png" },
+  { src: "/images/white.png" },
+];
+
 function App() {
   return (
     <div className="App">
-      <h1>Match</h1>
+      <h1>Canine Match</h1>
       <button>New Game</button>
     </div>
   );
