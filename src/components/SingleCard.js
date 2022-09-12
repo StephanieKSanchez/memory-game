6216ef51d85aeee6e840a750dcab45c059966293
Stephanie Sanchez <stephk006@gmail.com>
2022-09-12 14:21:59 -0400

    moved template to SingleCard component

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,5 +1,12 @@
 import "./SingleCard.css";
 
 export default function SingleCard() {
-  return <div></div>;
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={card.src} alt="card front" />
+        <img className="cover" src="/images/blank.png" alt="card cover" />
+      </div>
+    </div>
+  );
 }
